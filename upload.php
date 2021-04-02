c7a7654496b4a223e90a8bbded6fe057e0c6e11b
TristHub <57120755+TristHub@users.noreply.github.com>
2021-04-02 12:39:39 -0400

    Update upload.php

--- upload.php
+++ upload.php
@@ -12,4 +12,11 @@ if(isset($_POST["submit"])) {
     $uploadOk = 1;
   }
 }
+
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
 ?>
